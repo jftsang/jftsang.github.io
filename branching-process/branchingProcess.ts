8ef87aa1f4b8f81e479749f571044e30c01a32cc
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2024-01-21 23:17:33 +0000

    Goodies for branching processes

--- branching-process/branchingProcess.ts
+++ branching-process/branchingProcess.ts
@@ -1,13 +1,31 @@
 import {PoissonDistribution, GeometricDistribution, UniformDistribution, Distribution} from "./distributions";
 // import Plotly from "plotly.js-dist-min";
 
+const nSims = 100;
+const nGenerations = 20;
+const popCap = 10000;
+
 const form = document.querySelector("form") as HTMLFormElement;
 const parameterInput = document.getElementById("parameter") as HTMLInputElement;
-const generationsInput = document.getElementById("generations") as HTMLInputElement;
+const initialInput = document.getElementById("initial") as HTMLInputElement;
 const runButton = document.getElementById("run") as HTMLButtonElement;
+
+const generationSlider = document.getElementById("generationSlider") as HTMLInputElement;
+generationSlider.max = (nGenerations - 1).toString();
+const logScaleCheckbox = document.getElementById("logScale") as HTMLInputElement;
+
+
+function nextGeneration(currentGeneration: number, distribution: Distribution): number {
+  if (currentGeneration > popCap) {
+    return currentGeneration;
+  }
+  return distribution.sumOfIndependentSamples(currentGeneration);
+}
+
 
 runButton.addEventListener("click", (evt) => {
   evt.preventDefault();
+
   const formData = new FormData(form);
   for (const [name, value] of formData.entries()) {
     console.log(`${name}: ${value}`);
@@ -15,6 +33,7 @@ runButton.addEventListener("click", (evt) => {
 
   const distributionName = formData.get("distribution") as string;
   const parameter = parseFloat(parameterInput.value);
+  const initial = parseInt(initialInput.value);
 
   let distribution: Distribution;
   if (distributionName === "uniform")
@@ -26,20 +45,59 @@ runButton.addEventListener("click", (evt) => {
   else
     throw new Error(`Unknown distribution ${distributionName}`);
 
-  const generations = parseInt(generationsInput.value);
+  const results: number[][] = [];
 
-  const x = [];
-  for (let i = 0; i < 500; i ++) {
-    x[i] = distribution.sample();
+  results.push(new Array(nSims).fill(initial));
+
+  for (let gen = 1; gen < nGenerations; gen++) {
+    const lastGen = results[gen - 1];
+    const thisGen = lastGen.map(prevPop => nextGeneration(prevPop, distribution));
+    results.push(thisGen);
   }
 
-  const trace = {
-    x: x,
-    type: 'histogram',
-  };
-  const data = [trace];
+  function drawHistogram(gen: number, log: boolean = false) {
+    const res = log ? results[gen].filter(x => x > 0) : results[gen];
 
-  const histogram = document.getElementById("histogram");
-  // @ts-ignore
-  Plotly.newPlot(histogram, data);
+    const maxX = Math.max(...res);
+
+    // @ts-ignore
+    const trace = {
+      x: res,
+      type: 'histogram',
+      histfunc: 'count',
+      xbins: {
+        start: 0,
+        end: log ? Math.ceil(Math.log10(maxX)) : maxX,
+        size: 1
+      },
+      marker: {
+        color: 'rgba(100, 200, 150, 0.7)'
+      }
+    };
+    // @ts-ignore
+    const data = [trace];
+    const layout = {
+      title: 'Branching process',
+      xaxis: {
+        title: 'Population',
+        type: log ? 'log' : 'linear'
+      },
+      yaxis: {
+        title: 'Probability',
+      }
+    };
+
+    const histogram = document.getElementById("histogram") as HTMLDivElement;
+    // @ts-ignore
+    Plotly.newPlot(histogram, data, layout);
+  }
+
+  const redraw = () => {
+    drawHistogram(parseInt(generationSlider.value), logScaleCheckbox.checked);
+  }
+  generationSlider.oninput = redraw;
+  logScaleCheckbox.oninput = redraw;
+
+  generationSlider.value = (1).toString();
+  drawHistogram(1);
 })
--- branching-process/distributions.ts
+++ branching-process/distributions.ts
@@ -1,22 +1,33 @@
-export interface Distribution {
-  sample: () => number;
+export abstract class Distribution {
+  abstract sample(): number;
+
+  sumOfIndependentSamples(n: number): number {
+    return Array.from({length: n}, () => this.sample())
+      .reduce((a, b) => a + b, 0);
+  }
 }
 
-export class UniformDistribution implements Distribution {
-  constructor(private min: number, private max: number) {}
+export class UniformDistribution extends Distribution {
+  constructor(private min: number, private max: number) {
+    super();
+  }
+
   sample() {
     return Math.floor(Math.random() * (this.max + 1 - this.min) + this.min);
   }
 }
 
-export class PoissonDistribution implements Distribution {
+export class PoissonDistribution extends Distribution {
   private readonly l: number;
+
   constructor(private lambda: number) {
+    super();
     if (this.lambda <= 0) {
       throw new Error("lambda must be > 0");
     }
     this.l = Math.exp(-this.lambda);
   }
+
   sample() {
     let k = 0
     let q = 1
@@ -24,17 +35,24 @@ export class PoissonDistribution implements Distribution {
       k++;
       q *= Math.random();
     }
-    q = 1;
     return k - 1;
   }
+
+  sumOfIndependentSamples(n: number) {
+    if (n === 0) return 0;
+    return (new PoissonDistribution(this.lambda * n)).sample();
+  }
+
 }
 
-export class GeometricDistribution implements Distribution {
+export class GeometricDistribution extends Distribution {
   constructor(private p: number) {
+    super();
     if (this.p <= 0 || this.p > 1) {
       throw new Error("need 0 < p <= 1");
     }
   }
+
   sample() {
     let failures = 0;
     while (Math.random() > this.p) {
